1. Fix implementation of curved trajectory calculation. 2. Fix plotting of velocity-time graph and angular-velocity-time graph. 3. Implement "animated" plotting of the robot's displacement following the trajectory of the robot as a function of time.

kinematic.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 import random
+import time
 
 class Trajectory:
 
@@ -20,26 +21,29 @@ class Trajectory:
         self.add_waypoint(init_pos[0], init_pos[1])
         self.add_waypoint(final_pos[0], final_pos[1])
 
+        # Initialize the initial unit vector (the unit vector for the initial
+        # velocity at initial point).
         self.__init_u = np.array([
-            [1 * math.cos(math.pi * orientation / 180.0)],
-            [1 * math.sin(math.pi * orientation / 180.0)]
-        ])
-
-        self.__init_v = np.array([
-            [speed * math.cos(math.pi * orientation / 180.0)],
-            [speed * math.sin(math.pi * orientation / 180.0)]
+            [math.cos(self.__init_orientation)],
+            [math.sin(self.__init_orientation)]
         ])
 
     def next(self):
         self.__i = self.__i + 1
+
+    def next_waypoint(self):
+        return self.__waypoints[self.__i + 1]
+
+    def is_final_waypoint(self):
+        return self.__i == len(self.__waypoints) - 1
 
     def add_waypoint(self, x, y):
         self.__waypoints.append(np.array([[x], [y]]))
 
     def param_a(self):
 
-        curr_v = self.unit_velocity_at_point(self.__i)
-        next_v = self.unit_velocity_at_point(self.__i + 1)
+        curr_v = self.unit_velocity_at_point(self.__i) * self.__speed
+        next_v = self.unit_velocity_at_point(self.__i + 1) * self.__speed
 
         time_estimate = self.estimate_time_between_points(self.__speed, self.__i)
 
@@ -53,8 +57,8 @@ class Trajectory:
 
     def param_b(self):
 
-        curr_v = self.unit_velocity_at_point(self.__i)
-        next_v = self.unit_velocity_at_point(self.__i + 1)
+        curr_v = self.unit_velocity_at_point(self.__i) * self.__speed
+        next_v = self.unit_velocity_at_point(self.__i + 1) * self.__speed
 
         time_estimate = self.estimate_time_between_points(self.__speed, self.__i)
 
@@ -71,22 +75,84 @@ class Trajectory:
         a = self.param_a()
         b = self.param_b()
 
-        v0 = self.unit_velocity_at_point(self.__i)
+        v0 = self.unit_velocity_at_point(self.__i) * self.__speed
 
         v = 0.5 * a * (t ** 2) + b * (t) + v0
 
         w = (a[1] * t + b[1]) * v[0] - (a[0] * t + b[0]) * v[1]
         w = w / ((v[0] ** 2) + (v[1] ** 2))
 
-        forward_speed = np.linalg.norm(v)
-        angular_speed = float(w)
+        return np.linalg.norm(v), float(w)
 
-        return forward_speed, angular_speed
+    def plot(self, delta_time):
 
-    def frange(x, y, step):
-        while x < y:
-            yield x
-            x += step
+        # Waypoints.
+        waypoint_x = [] 
+        waypoint_y = []
+        for wp in self.__waypoints:
+            waypoint_x.append(wp[0])
+            waypoint_y.append(wp[1])
+
+        # Velocity at each waypoint.
+        vel_at_pt_x = []
+        vel_at_pt_y = []
+        for i, wp in enumerate(self.__waypoints):
+
+            vel = self.unit_velocity_at_point(i)
+
+            p0 = wp - vel
+            p1 = wp + vel
+
+            vel_at_pt_x.append([p0[0], p1[0]]) 
+            vel_at_pt_y.append([p0[1], p1[1]]) 
+
+        t0 = 0
+        time_estimate = self.estimate_time_between_points(
+            self.__speed, self.__i)
+        end_time = time_estimate
+
+        pos_x = self.__waypoints[0][0]
+        pos_y = self.__waypoints[0][1]
+        displacement_x = [pos_x]
+        displacement_y = [pos_y]
+        theta = self.__init_orientation
+
+        timestep = delta_time
+
+        plt.ion()
+
+        # Init fig.
+        fig = plt.figure()
+        ax = fig.add_subplot(111, xlim=[0, 100], ylim=[0, 100])
+        line1, = ax.plot(displacement_x, displacement_y, 'r-')
+
+        while True:
+
+            if timestep >= time_estimate:
+                self.next()
+
+                if self.__i == len(self.__waypoints) - 1:
+                    break
+
+                time_estimate = self.estimate_time_between_points(
+                    self.__speed, self.__i)
+                end_time = end_time + time_estimate
+                t0 = timestep
+
+            v, w = self.motion(timestep - t0)
+            theta = theta + w
+            pos_x = pos_x + v * delta_time * math.cos(theta)
+            pos_y = pos_y + v * delta_time * math.sin(theta)
+            displacement_x.append(pos_x)
+            displacement_y.append(pos_y)
+
+            line1.set_xdata(displacement_x)
+            line1.set_ydata(displacement_y)
+            fig.canvas.draw()
+            fig.canvas.flush_events()
+
+            timestep += delta_time
+            time.sleep(delta_time)
 
     def plot_trajectory(self):
 
@@ -115,9 +181,6 @@ class Trajectory:
             vector_at_point_x.append([p0[0], p1[0]])
             vector_at_point_y.append([p0[1], p1[1]])
 
-        segment_line_x = []
-        segment_line_y = []
-
         t0 = 0
         time_estimate = self.estimate_time_between_points(
             self.__speed, self.__i)
@@ -127,25 +190,26 @@ class Trajectory:
         dis_y = self.__waypoints[0][1]
         curve_line_x = [dis_x]
         curve_line_y = [dis_y]
-        segment_line_x.append(curve_line_x[-1])
 
         theta = self.__init_orientation
-        timestep = 1
+        timestep = 0
         while True:
             if timestep >= end_time:
-                segment_line_x.append(curve_line_x[-1])
                 self.next()
 
                 if self.__i == len(self.__waypoints) - 1:
                     break
-
+                
                 time_estimate = self.estimate_time_between_points(
                     self.__speed, self.__i)
                 end_time = end_time + time_estimate
                 t0 = timestep
 
             v, w = self.motion(timestep - t0)
-            theta = (theta + w)
+            d_x = 0
+            d_y = 0
+
+            theta = theta + w
             dis_x = dis_x + v * delta_time * math.cos(theta)
             dis_y = dis_y + v * delta_time * math.sin(theta)
             curve_line_x.append(dis_x)
@@ -161,9 +225,6 @@ class Trajectory:
         for i in range(len(vector_at_point_x)):
             plt.plot(vector_at_point_x[i], vector_at_point_y[i], 'c--')
 
-        for x_coords in segment_line_x:
-            plt.axvline(x=x_coords, linestyle='--', color='gray')
-
         init_pos = (float(self.__init_pos[0]), float(self.__init_pos[1]))
         init_orientation = 180.0 * self.__init_orientation / math.pi
         targ_pos = (float(self.__targ_pos[0]), float(self.__targ_pos[1]))
@@ -173,6 +234,54 @@ class Trajectory:
                 ' degree.'
                 % (init_pos, init_orientation, targ_pos,\
                     targ_orientation))
+        plt.show()
+
+        self.__i = temp
+
+    def plot_motion(self):
+
+        temp = self.__i
+
+        time_estimate = self.estimate_time_between_points(
+            self.__speed, self.__i)
+        end_time = time_estimate
+
+        t0 = 0
+        timestep = 0
+        delta_time = 0.5
+
+        v_plot = []
+        w_plot = []
+        t_plot = []
+
+        fig = plt.figure()
+        ax1 = fig.add_subplot(121)
+        ax2 = fig.add_subplot(122)
+
+        while True:
+            if timestep >= end_time:
+                
+                self.next()
+
+                if self.__i == len(self.__waypoints) - 1:
+                    break
+
+                time_estimate = self.estimate_time_between_points(
+                    self.__speed, self.__i)
+                end_time = end_time + time_estimate
+                t0 = timestep
+
+            v, w = self.motion(timestep - t0)
+            v_plot.append(v)
+            w_plot.append(w)
+
+            timestep += delta_time
+            t_plot.append(timestep)
+
+        ax1.plot(t_plot, v_plot, 'rx-')
+        ax1.set_title('Linear-velocity-time graph.')
+        ax2.plot(t_plot, w_plot, 'ko-')
+        ax2.set_title('Angular-velocity-time graph')
         plt.show()
 
         self.__i = temp
@@ -248,3 +357,5 @@ class Trajectory:
 
             s += str((x, y)) + ', '
         return s
+
+