1. Fixed more bugs of the trajectory calculation implementation i.e., division by zero problems, domain out of range problems.
2. Implement robot displacement plots. Now the robot's displacement
overtime can be compared with the calculated displacement over time (trajectory).

kinematic.py
@@ -6,27 +6,32 @@ import time
 
 class Trajectory:
 
-    def __init__(self, speed, init_pos, orientation, 
-            final_pos, final_orientation):
+    def __init__(self, speed, init_pos, init_orientation, 
+            next_pos, final_orientation=None):
 
         self.__i = 0
-        self.__init_orientation = math.pi * orientation / 180.0
-        self.__targ_orientation = math.pi * final_orientation / 180.0
-        self.__init_pos = np.array([init_pos]).T
-        self.__targ_pos = np.array([final_pos]).T
+        self.__init_orientation = math.radians(init_orientation)
+        self.__targ_orientation = final_orientation
+
+        self.__init_pos = np.array(init_pos)
+        self.__targ_pos = np.array(next_pos)
+
         self.__waypoints = []
 
         self.__speed = speed 
 
         self.add_waypoint(init_pos[0], init_pos[1])
-        self.add_waypoint(final_pos[0], final_pos[1])
+        self.add_waypoint(next_pos[0], next_pos[1])
 
         # Initialize the initial unit vector (the unit vector for the initial
         # velocity at initial point).
         self.__init_u = np.array([
-            [math.cos(self.__init_orientation)],
-            [math.sin(self.__init_orientation)]
+            math.cos(self.__init_orientation),
+            math.sin(self.__init_orientation)
         ])
+
+    def current(self):
+        return self.__i
 
     def next(self):
         self.__i = self.__i + 1
@@ -38,7 +43,7 @@ class Trajectory:
         return self.__i == len(self.__waypoints) - 1
 
     def add_waypoint(self, x, y):
-        self.__waypoints.append(np.array([[x], [y]]))
+        self.__waypoints.append(np.array([x, y]))
 
     def param_a(self):
 
@@ -70,7 +75,46 @@ class Trajectory:
 
         return b
 
+    def is_straight_trajectory(self, i):
+        
+        """
+        Determines if the trajectory from waypoint i to waypoint i+1 is a
+        straight line. First, the unit vector at each waypoint i and i+1 is
+        compared to see if they share the same direction. Next, the unit vector
+        at i+1 and the direction vector from waypoint i to waypoint i+1 is
+        compared, again, to see if they share the same direction. If both of the
+        aforementioned comparisons shows that all the vectors shares the same
+        direction, then the path of the trajectory from waypoint i to waypoint
+        i+1 is a straight line.
+        """
+
+        curr_dir = self.unit_velocity_at_point(i)
+        next_dir = self.unit_velocity_at_point(i + 1)
+
+        curr_wp = self.__waypoints[i]
+        next_wp = self.__waypoints[i + 1]
+
+        dir_vec = next_wp - curr_wp
+        dir_vec = dir_vec / np.linalg.norm(dir_vec)
+
+        dot_1 = np.dot(curr_dir, next_dir)
+        dot_2 = np.dot(next_dir, dir_vec)
+
+        return abs(dot_1 - 1) < 0.0000001 and abs(dot_2 - 1) < 0.0000001
+
     def motion(self, t):
+
+        """
+        Get the motion of the robot at time t during the trajectory between two
+        waypoints. This assumes the time since the previous waypoint is t0, and
+        t = t_current - t0.
+
+        The motion returned at time t depends on the trajectory to get from the
+        current waypoint to the next waypoint.
+        """
+
+        if self.is_straight_trajectory(self.__i):
+            return self.__speed, 0.0
 
         a = self.param_a()
         b = self.param_b()
@@ -157,6 +201,7 @@ class Trajectory:
     def plot_trajectory(self):
 
         temp = self.__i
+        self.__i = 0
 
         delta_time = 1
         
@@ -177,6 +222,10 @@ class Trajectory:
 
             p0 = wp - velocity_at_point
             p1 = wp + velocity_at_point
+
+            # Matplotlib is sensitive!
+            p0[abs(p0) < 0.0000001] = 0
+            p1[abs(p1) < 0.0000001] = 0
 
             vector_at_point_x.append([p0[0], p1[0]])
             vector_at_point_y.append([p0[1], p1[1]])
@@ -211,7 +260,14 @@ class Trajectory:
 
             theta = theta + w
             dis_x = dis_x + v * delta_time * math.cos(theta)
+
+            # Again, Matplotlib is sensitive!
+            if abs(dis_x) < 0.0000001:
+                dis_x = 0
             dis_y = dis_y + v * delta_time * math.sin(theta)
+            if abs(dis_y) < 0.0000001:
+                dis_y = 0
+
             curve_line_x.append(dis_x)
             curve_line_y.append(dis_y)
 
@@ -225,15 +281,15 @@ class Trajectory:
         for i in range(len(vector_at_point_x)):
             plt.plot(vector_at_point_x[i], vector_at_point_y[i], 'c--')
 
-        init_pos = (float(self.__init_pos[0]), float(self.__init_pos[1]))
-        init_orientation = 180.0 * self.__init_orientation / math.pi
-        targ_pos = (float(self.__targ_pos[0]), float(self.__targ_pos[1]))
-        targ_orientation = 180.0 * self.__targ_orientation / math.pi
-        plt.title('Calculated trajectory with initial position %s and '\
-                'orientation %s degree, final position %s and orientation %s'\
-                ' degree.'
-                % (init_pos, init_orientation, targ_pos,\
-                    targ_orientation))
+        # init_pos = (float(self.__init_pos[0]), float(self.__init_pos[1]))
+        # init_orientation = 180.0 * self.__init_orientation / math.pi
+        # targ_pos = (float(self.__targ_pos[0]), float(self.__targ_pos[1]))
+        # targ_orientation = 180.0 * self.__targ_orientation / math.pi
+        # plt.title('Calculated trajectory with initial position %s and '\
+        #         'orientation %s degree, final position %s and orientation %s'\
+        #         ' degree.'
+        #         % (init_pos, init_orientation, targ_pos,\
+        #             targ_orientation))
         plt.show()
 
         self.__i = temp
@@ -288,6 +344,13 @@ class Trajectory:
 
     def estimate_time_between_points(self, speed, i):
 
+        if self.is_straight_trajectory(i):
+            p0 = self.__waypoints[i]
+            p1 = self.__waypoints[i + 1]
+            dist = np.linalg.norm(p1 - p0)
+
+            return dist / float(speed)
+
         next_line = self.__waypoints[i + 1] - self.__waypoints[i]
         next_line_norm = np.linalg.norm(next_line)
 
@@ -297,21 +360,30 @@ class Trajectory:
         next_v = self.unit_velocity_at_point(i + 1)
         next_v_norm = np.linalg.norm(next_v)
 
-        theta1 = np.dot(next_line.flatten(), curr_v.flatten()) /\
-                (next_line_norm * curr_v_norm)
+        theta1 = np.dot(next_line, curr_v) / float(next_line_norm * curr_v_norm)
+
+        # Ensure theta1 is within the domain of acos.
+        if abs(theta1 - 1) > 1e-18:
+            theta1 = theta1 // abs(theta1)
+
         theta1 = math.acos(theta1)
 
-        if theta1 == 0:
-            theta1 = 0.00001
+        # Cheat a bit.
+        if abs(theta1) < 0.000000001:
+            theta1 = 0.00000001
 
         len1 = abs((abs(next_line_norm) * theta1) / math.sin(theta1)) 
 
-        theta2 = np.dot(next_line.flatten(), next_v.flatten()) /\
-                (next_line_norm * next_v_norm)
+        theta2 = np.dot(next_line, next_v) / (next_line_norm * next_v_norm)
+
+        if abs(theta2 - 1) > 1e-18:
+            theta2 = theta2 // abs(theta2)
+
         theta2 = math.acos(theta2)
 
-        if theta2 == 0:
-            theta2 = 0.00001
+        # Cheat a bit.
+        if abs(theta2) < 0.000000001:
+            theta2 = 0.00000001
 
         len2 = abs((abs(next_line_norm) * theta2) / math.sin(theta2))
 
@@ -321,6 +393,12 @@ class Trajectory:
         estimate_time = (len1 + len2) / (speed1 + speed2)
 
         return estimate_time
+
+    def get_speed(self):
+        return self.__speed
+
+    def get_waypoints(self):
+        return self.__waypoints
         
     def unit_velocity_at_point(self, i):
 
@@ -328,10 +406,18 @@ class Trajectory:
             return self.__init_u
 
         if i == len(self.__waypoints) - 1:
-            return np.array([
-                [math.cos(self.__targ_orientation)],
-                [math.sin(self.__targ_orientation)]
-            ])
+            if self.__targ_orientation is not None:
+                return np.array([
+                    math.cos(self.__targ_orientation),
+                    math.sin(self.__targ_orientation)
+                ])
+            else:
+                # Compute the final orientation using the last two waypoints.
+                wp0 = self.__waypoints[i - 1]
+                wp1 = self.__waypoints[i]
+                direction = wp1 - wp0
+                direction = direction / np.linalg.norm(direction)
+                return direction
 
         p_curr = self.__waypoints[i]
         p_prev = self.__waypoints[i - 1]
@@ -347,15 +433,3 @@ class Trajectory:
         u = m / np.linalg.norm(m)
 
         return u
-
-    def waypoint_str(self):
-    
-        s = ''
-        for wp in self.__waypoints:
-            x = float(wp[0])
-            y = float(wp[1])
-
-            s += str((x, y)) + ', '
-        return s
-
-