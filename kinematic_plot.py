41f17de1afc7d615c5c681ea5cbf6b3ba53c5e64
Amirul Menjeni <amirulmenjeni@gmail.com>
2018-10-30 01:29:19 +0800

    1. Implement trajectory method that calculates the displacement of the robot as a function of time.
    2. Improve actual trajectory of the robot to follow the waypoints by
    recalculating the trajectory between waypoints.

---
+++ kinematic_plot.py
@@ -0,0 +1,4 @@
+from robot import Robot
+
+def plot_compare(speed, robot, next_pos, targ_orientation):
+    pass
--- robot.py
+++ robot.py
@@ -7,6 +7,7 @@ import threading
 import math
 import numpy as np
 import matplotlib.pyplot as plt 
+import matplotlib.gridspec as gridspec
 import csv
 from serial.tools import list_ports
 from kinematic import Trajectory
@@ -139,8 +140,8 @@ class PIDControler:
         return self.kp * self.__error
 
     def I(self, dt):
-        self.__integral = self.ki * (self.__integral + self.__error * dt)
-        return self.__integral
+        self.__integral = (self.__integral + self.__error * dt)
+        return self.ki * self.__integral
 
     def D(self, dt):
         derivative = (self.__error - self.__prev_error) / dt
@@ -148,11 +149,11 @@ class PIDControler:
 
 class StaticPlotter:
 
-    def __init__(self, n, init_pos, style='b-'):
+    def __init__(self, n, init_pos, style='b-', on_off=None):
 
         self.__n = n
         self.__theta = [0] * n
-        
+
         self.__pos_x = [p[0] for p in init_pos]
         self.__pos_y = [p[1] for p in init_pos]
 
@@ -167,19 +168,55 @@ class StaticPlotter:
             self.__styles = ['b-'] * n
         else:
             self.__styles = style
-        print(self.__styles)
 
-        self.__lines = []
+        if on_off is not None:
+            self.__on_off = on_off
+            for i in range(n):
+                if i not in self.__on_off:
+                    self.__on_off[i] = '111'
+        else:
+            self.__on_off = {}
+            for i in range(n):
+                self.__on_off[i] = '111'
 
         self.__min_x = min(self.__pos_x)
         self.__max_x = max(self.__pos_x)
         self.__min_y = min(self.__pos_y)
         self.__max_y = min(self.__pos_y)
+        self.__min_v = 999
+        self.__max_v = -999
+        self.__min_w = 999
+        self.__max_w = -999
+
+        self.__time = [0] * n
+        self.__timestep = []
+        for i in range(n):
+            self.__timestep.append([])
+        self.__linear_velocity = []
+        self.__angular_velocity = []
+
+        for i in range(n):
+            self.__linear_velocity.append([])
+            self.__angular_velocity.append([])
+
+        gs = gridspec.GridSpec(2, 2)
 
         self.__fig = plt.figure()
-        self.__ax = self.__fig.add_subplot(
-            111, 
-            xlim=[self.__min_x, self.__max_x], 
+
+        # Subplot for velocity-time graph.
+        self.__ax1 = self.__fig.add_subplot(
+            gs[0, 0],
+            ylim=[self.__min_v, self.__max_v])
+
+        # Subplot for angular-velocity-time graph.
+        self.__ax2 = self.__fig.add_subplot(
+            gs[0, 1],
+            ylim=[self.__min_w, self.__max_w])
+
+        # Subplot for displacement.
+        self.__ax3 = self.__fig.add_subplot(
+            gs[1, :],
+            xlim=[self.__min_x, self.__max_x],
             ylim=[self.__min_y, self.__max_y])
 
 
@@ -187,7 +224,15 @@ class StaticPlotter:
         return ax.plot(self.__pos_x[i], self.__pos_y[i],\
             self.__styles[i])[0]
 
-    def update_plot(self, i, delta_distance, delta_angle):
+    def set_waypoints(self, waypoints):
+        self.__waypoints_x = [0] * len(waypoints)
+        self.__waypoints_y = [0] * len(waypoints)
+
+        for i, (x, y) in enumerate(waypoints):
+            self.__waypoints_x[i] = x
+            self.__waypoints_y[i] = y
+
+    def update_plot(self, i, delta_time, delta_distance, delta_angle, v, w):
 
         if i < 0 or i > self.__n:
             raise ValueError('Invalid line index given.')
@@ -200,25 +245,82 @@ class StaticPlotter:
         self.__pos_y[i] = self.__pos_y[i] +\
             delta_distance * math.sin(self.__theta[i])
 
-        if self.__pos_x[i] < self.__min_x:
-            self.__min_x = self.__pos_x[i]
-        if self.__pos_x[i] > self.__max_x:
-            self.__max_x = self.__pos_x[i]
-        if self.__pos_y[i] < self.__min_y:
-            self.__min_y = self.__pos_y[i]
-        if self.__pos_y[i] > self.__max_y:
-            self.__max_y = self.__pos_y[i]
+        self.__timestep[i].append(self.__time[i])
+        self.__time[i] = self.__time[i] + delta_time
+
+        self.__linear_velocity[i].append(v)
+        self.__angular_velocity[i].append(w)
+
+        # print('v:', len(self.__linear_velocity[i]))
+        # print('t:', len(self.__timestep[i]))
+
+        if i in self.__on_off and self.__on_off[i][2] == '1':
+            if self.__pos_x[i] < self.__min_x:
+                self.__min_x = self.__pos_x[i]
+            if self.__pos_x[i] > self.__max_x:
+                self.__max_x = self.__pos_x[i]
+            if self.__pos_y[i] < self.__min_y:
+                self.__min_y = self.__pos_y[i]
+            if self.__pos_y[i] > self.__max_y:
+                self.__max_y = self.__pos_y[i]
+
+        if i in self.__on_off and self.__on_off[i][0] == '1':
+            if v < self.__min_v:
+                self.__min_v = v
+            if v > self.__max_v:
+                self.__max_v = v
+
+        if i in self.__on_off and self.__on_off[i][1] == '1':
+            if w < self.__min_w:
+                self.__min_w = w
+            if w > self.__max_w:
+                self.__max_w = w
 
         self.__dis_x[i].append(self.__pos_x[i])
         self.__dis_y[i].append(self.__pos_y[i])
 
     def draw(self):
 
-        self.__ax.set_xlim(xmin=self.__min_x, xmax=self.__max_x)
-        self.__ax.set_ylim(ymin=self.__min_y, ymax=self.__max_y)
+        v_margin = 0.05 * (self.__max_v - self.__min_v)
+        w_margin = 0.05 * (self.__max_w - self.__min_w)
+        x_margin = 0.05 * (self.__max_x - self.__min_x)
+        y_margin = 0.05 * (self.__max_y - self.__min_y)
+
+        self.__ax1.set_ylim(ymin=self.__min_v - v_margin, 
+                            ymax=self.__max_v + v_margin)
+        self.__ax2.set_ylim(ymin=self.__min_w - w_margin,
+                            ymax=self.__max_w + w_margin)
+        self.__ax3.set_xlim(xmin=self.__min_x - x_margin,
+                            xmax=self.__max_x + x_margin)
+        self.__ax3.set_ylim(ymin=self.__min_y - y_margin, 
+                            ymax=self.__max_y + y_margin)
+
+        self.__ax1.set_xlabel('Time')
+        self.__ax1.set_ylabel('Linear velocity')
+
+        self.__ax2.set_xlabel('Time')
+        self.__ax2.set_ylabel('Angular velocity')
+
+        self.__ax3.set_xlabel('X-displacement')
+        self.__ax3.set_ylabel('Y-displacement')
 
         for i in range(self.__n):
-            self.__ax.plot(self.__dis_x[i], self.__dis_y[i], self.__styles[i])
+            if i in self.__on_off:
+                if self.__on_off[i][0] == '1':
+                    self.__ax1.plot(self.__timestep[i], 
+                        self.__linear_velocity[i], self.__styles[i])
+                if self.__on_off[i][1] == '1':
+                    self.__ax2.plot(self.__timestep[i],
+                        self.__angular_velocity[i], self.__styles[i])
+                if self.__on_off[i][2] == '1':
+                    self.__ax3.plot(self.__dis_x[i], self.__dis_y[i],
+                        self.__styles[i])
+
+        # Waypoints.
+        if self.__waypoints_x is not None and\
+           self.__waypoints_y is not None:
+            self.__ax3.plot(self.__waypoints_x, self.__waypoints_y, 'bo-')
+
         plt.grid()
         plt.show()
 
@@ -298,7 +400,8 @@ class AnimPlotter:
 
 class Robot:
 
-    def __init__(self, b=26, port='', max_speed=10):
+    def __init__(self, b=26, port='', max_speed=10,
+        pid_v=(0, 0, 0), pid_w=(0, 0, 0)):
 
         """
         Initialize the Robot class. 
@@ -350,8 +453,10 @@ class Robot:
         self.__pose = (0, 0, 0)
 
         # PID controllers.
-        self.pid_v = PIDControler(0.00, 0.01, 0.01)
-        self.pid_w = PIDControler(0.01, 0.01, 0.01)
+        # self.pid_v = PIDControler(1.5, 0.015, 0.0)
+        # self.pid_w = PIDControler(0.65, 0.05, 0.0)
+        self.pid_v = PIDControler(pid_v[0], pid_v[1], pid_v[2])
+        self.pid_w = PIDControler(pid_w[0], pid_w[1], pid_w[2])
 
         # Flags.
         self.is_pid_enable = True
@@ -364,8 +469,13 @@ class Robot:
         self.auto_end_time = 0
         self.auto_t0 = 0
 
-        self.plotter = StaticPlotter(2,
-            [self.get_pose()[:2]] * 2, ['kx--', 'cx--'])
+        self.plotter = StaticPlotter(
+            3, # Plot 3 lines for each graph.
+            [self.get_pose()[:2]] * 3, 
+            ['kx-', 'cx--', 'g^--'],
+            {0: '111',
+             1: '111',
+             2: '000'}) # Plot number 2 is not drawn
 
         self.__is_timestep_end = False
 
@@ -633,7 +743,8 @@ class Robot:
         self.auto_timestep = 0
         self.auto_t0 = 0
 
-    def drive_trajectory(self, speed, waypoints, targ_orientation=None):
+    def drive_trajectory(self, speed, waypoints, targ_orientation=None,
+        is_recalculation=False):
 
         self.auto_trajectory = Trajectory(speed,
             self.__pose[:2], self.__pose[2],
@@ -654,8 +765,14 @@ class Robot:
 
         self.is_autonomous = True
         self.is_drive = True
+        self.issued_v = None
+        self.issued_w = None
         self.auto_timestep = 0
         self.auto_t0 = 0
+        self.auto_speed = speed
+
+        if not is_recalculation:
+            self.plotter.set_waypoints([self.__pose[:2]] + waypoints)
 
     def stop_thread(self, i):
         
@@ -707,7 +824,9 @@ class Robot:
         Keep reading the encoder values on the robot's pose.
         """
 
-        delta_time = 0.5
+        delta_time = 1
+        prev_traj_v = None
+        prev_traj_w = None
 
         while True:
 
@@ -721,6 +840,7 @@ class Robot:
                 delta_distance, delta_angle = self.get_sensor(PKT_MOTION)
             except:
                 pass
+
             # Compute the linear and angular velocity from measured distance and
             # angle within delta_time respectively.
             v = delta_distance / delta_time # Forward velocity
@@ -730,61 +850,83 @@ class Robot:
             self.__delta_angle = delta_angle
 
             if self.is_drive:
-                print('v, w:', v, w)
+                # print('v, w:', v, w)
+                pass
 
-            # Update linear and angular velocity member variables.
-            self.__v = v
-            self.__w = w
+            read_v = v
+            read_w = w
 
             # Update the position of the robot.
             self.__update_position(delta_distance, delta_angle)
 
-
             v1, v2 = 0, 0
-
             if self.is_autonomous:
 
-                v, w = self.auto_trajectory.motion(
-                    self.auto_timestep - self.auto_t0)
+                if self.auto_timestep >= self.auto_end_time / 4.0:
 
-                v1, v2 = Robot.__inverse_drive(v, w, self.__b)
-                # print('[%s] auto v, w:' % (self.auto_timestep), v, w)
-                # print('[%s] autov1,v2:' % (self.auto_timestep), v1, v2)
+                    next_pos = self.auto_trajectory.displacement(
+                        self.auto_end_time)
 
-                self.plotter.update_plot(0, delta_distance, delta_angle)
-                self.plotter.update_plot(1, v * delta_time,
-                    math.degrees(w) * delta_time)
+                    if not Util.is_in_circle(next_pos, 15.0,
+                        self.__pose[:2]):
 
-                if self.auto_timestep >= self.auto_end_time:
-                    self.test_song()
-                    self.auto_trajectory.next()
-
-                    if self.auto_trajectory.is_final_waypoint():
                         self.is_autonomous = False
                         self.auto_timestep = 0
-                        v1, v2 = 0, 0
+                        waypoints = self.auto_trajectory.get_waypoints()[\
+                                self.auto_trajectory.current() + 1:]
+
+                        self.drive_trajectory(self.auto_speed, waypoints,
+                            is_recalculation=True)
+
                     else:
-                        time_estimate =\
-                            self.auto_trajectory.estimate_time_between_points(
-                                self.auto_trajectory.get_speed(),
-                                self.auto_trajectory.current())
-                        self.auto_end_time = self.auto_end_time + time_estimate 
-                        self.auto_t0 = self.auto_timestep
-                else:
+                        self.test_song()
+                        self.auto_trajectory.next()
+
+                        if self.auto_trajectory.is_final_waypoint():
+                            print('Final!')
+                            self.is_autonomous = False
+                            self.auto_timestep = 0
+                            v1, v2 = 0, 0
+                        else:
+                            time_estimate =\
+                                self.auto_trajectory.estimate_time_between_points(
+                                    self.auto_trajectory.get_speed(),
+                                    self.auto_trajectory.current())
+                            self.auto_end_time = self.auto_end_time + time_estimate 
+                            self.auto_t0 = self.auto_timestep
+
+                if not self.auto_trajectory.is_final_waypoint():
+
+                    input_v, input_w = self.auto_trajectory.motion(
+                        self.auto_timestep - self.auto_t0)
+
+                    v1, v2 = Robot.__inverse_drive(
+                        input_v, input_w, self.__b)
+
+                    # Update sensor reading plot.
+                    self.plotter.update_plot(0, delta_time, 
+                        delta_distance, delta_angle,
+                        read_v, read_w)
+
+                    # Update trajectory calculation plot.
+                    self.plotter.update_plot(1, delta_time, 
+                        input_v * delta_time,
+                        math.degrees(input_w) * delta_time,
+                        input_v, math.degrees(input_w))
+
+                    # self.plotter.update_plot(2, delta_time, 
+                    #     controlled_v * delta_time, math.degrees(w) * delta_time,
+                    #     controlled_v, math.degrees(controlled_w))
+
                     self.auto_timestep = self.auto_timestep + delta_time
 
-                    next_waypoint = self.auto_trajectory.next_waypoint()
-                    if Util.is_in_circle(next_waypoint, 0.5, self.__pose[:2]):
-                        print('End auto pilot. Destination reached.')
-                        self.is_autonomous = False
-                        self.auto_timestep = 0
+                self.drive_direct(v1, v2)
 
             else:
                 # Manual driving.
                 v1, v2 = Robot.__inverse_drive(
                     self.issued_v, self.issued_w, self.__b)
-
-            self.drive_direct(v1, v2)
+                self.drive_direct(v1, v2)
 
             if self.is_thread_stop_requested[THREAD_MOTION]:
                 break
@@ -947,3 +1089,7 @@ class Robot:
     def __inverse_drive(v, w, b):
 
         return v - b * w, v + b * w
+
+    def __forward_drive(v1, v2, b):
+
+        return ((v1 + v2) / 2.0), (v1 - v2) / (2.0 * b)
