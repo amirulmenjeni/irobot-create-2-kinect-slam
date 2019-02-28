1. Fix referencing error of particle's variables when resampling. 2. Change heading representation from degrees to radians.

robot.py
@@ -1,4 +1,5 @@
 import serial
+import sys
 import time
 import datetime
 import struct
@@ -42,7 +43,11 @@ PKT_BYTES[PKT_STATUS]   = 10
 
 # Threads
 THREAD_MOTION = 0
-THREAD_SLAM = 1
+THREAD_KINECT = 1
+
+MOTION_STATIC = 0
+MOTION_EXPLORE = 1
+MOTION_FOLLOW = 2
 
 class PIDController:
 
@@ -350,6 +355,7 @@ class Robot:
         self.auto_timestep = 0
         self.auto_end_time = 0
         self.auto_t0 = 0
+        self.motion_state = MOTION_STATIC
 
         self.plotter = StaticPlotter(
             3, # Plot 3 lines for each graph.
@@ -363,13 +369,21 @@ class Robot:
 
         # SLAM related.
         self.occ_grid_map = np.full(config.GRID_MAP_SIZE, 0.5)
-        self.vel_ctrl = np.array([0, 0]) # The control u_t
+
         self.particle_filter = slam.ParticleFilter(
-            config.CONTROL_DELTATIME,\
+            config.CONTROL_DELTA_TIME,\
             self.occ_grid_map.shape,\
             config.GRID_MAP_RESOLUTION,\
-            num_particles=100)
-        self.curr_scan = None
+            num_particles=config.PF_NUM_PARTICLES)
+
+        self.fast_slam = slam.FastSLAM(\
+            self.occ_grid_map.shape,
+            config.GRID_MAP_RESOLUTION,
+            config.CONTROL_DELTA_TIME,
+            num_particles=config.PF_NUM_PARTICLES)
+
+        self.u_t = None
+        self.z_t = None
 
         # Initialize all threads.
         self.__init_threads()
@@ -401,8 +415,8 @@ class Robot:
         self.threads = [
                 threading.Thread(target=Robot.thread_motion2, args=(self,),\
                 name='Motion'),\
-                threading.Thread(target=Robot.thread_slam, args=(self,),\
-                name='SLAM'),\
+                threading.Thread(target=Robot.thread_kinect, args=(self,),\
+                name='Kinect'),\
         ]
 
         # A list of flag to maintain thread stop request
@@ -716,65 +730,66 @@ class Robot:
         if not self.threads[i].is_alive():
             self.threads[i].start()
 
-    def thread_slam(self):
+    def thread_kinect(self):
 
         delta_time = 0.25
         SLICE_ROW = 315
         DEPTH_SHAPE_COL = 640
         RESOLUTION = config.GRID_MAP_RESOLUTION
+        MAP_SIZE = self.occ_grid_map.shape
 
         prev_cells = None
-        prev_pose = None
-
-        time.sleep(1.5)
 
         while 1:
 
+            while self.z_t is not None:
+                time.sleep(1e-6)
+
             # Get the x-y-locations of obstacles projected from kinect's depth
             # map.
-            frame_local = self.get_kinect_xy(SLICE_ROW)
-            self.curr_scan = frame_local
+            frame_local, depth_data = slam.get_kinect_xy(SLICE_ROW)
+            self.z_t = frame_local
 
-            # The pose of the robot when the this scan takes place.
-            curr_pose = self.get_pose()
+            # # The pose of the robot when the this scan takes place.
+            # curr_pose = self.get_pose()
 
-            # Transform the 2d coordinates in the robot's local coordinate
-            # system to the global coordinate system.
-            H = rutil.rigid_trans_mat3(curr_pose)
-            frame_global = rutil.transform_pts_2d(H, frame_local)
+            # # Transform the 2d coordinates in the robot's local coordinate
+            # # system to the global coordinate system.
+            # H = rutil.rigid_trans_mat3(curr_pose)
+            # frame_global = rutil.transform_pts_2d(H, frame_local)
  
-            # Convert the real 2d end-points into cell position (row, column)
-            # on the grid map.
-            end_pt_cells = slam.world_frame_to_cell_pos(frame_global,
-                self.occ_grid_map.shape, RESOLUTION)
+            # # Convert the real 2d end-points into cell position (row, column)
+            # # on the grid map.
+            # end_pt_cells = slam.world_frame_to_cell_pos(frame_global,
+            #     MAP_SIZE, RESOLUTION)
 
-            # Get the cells on the occupancy grid map that is observed in the
-            # current scan.
-            pos_cell = slam.world_to_cell_pos(curr_pose[:2],\
-                self.occ_grid_map.shape, RESOLUTION)
-            curr_cells, _ = slam.observed_cells(pos_cell, end_pt_cells)
+            # # Get the cells on the occupancy grid map that is observed in the
+            # # current scan.
+            # pos_cell = slam.world_to_cell_pos(curr_pose[:2], MAP_SIZE,
+            #         RESOLUTION)
+            # obs_dict  = slam.observed_cells(pos_cell, end_pt_cells)
 
-            # Scan-match the current scan frame with the previous scan frame.
-            if prev_cells is not None:
-                end_pt_cells = slam.scan_match(curr_cells, prev_cells,\
-                    end_pt_cells)
+            # obs_mat = slam.observation_matrix(obs_dict, occu=0.9, free=-0.7)
+            # curr_cells = obs_mat[:,:2]
 
-            # Update posterior.
-            slam.occupancy_grid_mapping(self.occ_grid_map,\
-                    np.copy(self.occ_grid_map), curr_pose, end_pt_cells,\
-                    RESOLUTION)
+            # # Scan-match the current scan frame with the previous scan frame.
+            # if prev_cells is not None:
+            #     end_pt_cells = slam.scan_match(curr_cells, prev_cells,\
+            #         end_pt_cells)
 
-            prev_cells = np.copy(curr_cells)
-            prev_pose = np.copy(curr_pose)
+            # # Update posterior.
+            # slam.update_occupancy_grid_map(self.occ_grid_map, obs_mat)
 
-            time.sleep(0.25)
+            # prev_cells = np.copy(curr_cells)
 
-            if self.is_thread_stop_requested[THREAD_SLAM]:
+            # time.sleep(config.MEASURE_DELTA_TIME)
+
+            if self.is_thread_stop_requested[THREAD_KINECT]:
                 break
 
     def thread_motion2(self):
 
-        delta_time = 0.25
+        delta_time = config.CONTROL_DELTA_TIME
         next_waypoint = None
         calc_pos = None
         prev_calc_pos = None
@@ -788,8 +803,6 @@ class Robot:
         pid_a = PIDController(0.0, 0, 0)
 
         self.auto_timestep = self.auto_timestep + delta_time * 5
-
-        print('Thread motion 2 started')
 
         while True:
 
@@ -810,9 +823,11 @@ class Robot:
             read_w = delta_angle / delta_time
 
             self.__delta_distance = delta_distance
-            self.__delta_angle = delta_angle
+            self.__delta_angle = math.radians(delta_angle)
 
-            self.__update_odometry(delta_distance, delta_angle)
+            self.__update_odometry(delta_distance, self.__delta_angle)
+
+            tstart = time.time()
 
             v1, v2 = 0, 0
 
@@ -889,7 +904,7 @@ class Robot:
 
                 pid_x.e(calc_pos[0], curr_pos[0])
                 pid_y.e(calc_pos[1], curr_pos[1])
-                pid_a.e(rutil.disp_to_angle(calc_pos), a, mod=360)
+                pid_a.e(rutil.disp_to_angle(calc_pos), a, mod=math.pi*2)
 
                 out_x = error_vector[0] +\
                     pid_x.P() + pid_x.I(delta_time) + pid_x.D(delta_time)
@@ -908,7 +923,7 @@ class Robot:
 
                 if step_counter % step_mod == 0:
 
-                    self.vel_ctrl = np.array([out_v, out_w])
+                    self.u_t = np.array([out_v, out_w])
 
                     v1, v2 = Robot.__inverse_drive(
                         out_v, out_w, self.__b)
@@ -916,17 +931,22 @@ class Robot:
 
                 timestep += delta_time
 
-                if self.curr_scan is not None:
-                    self.particle_filter.update(self.curr_scan, self.vel_ctrl,\
-                        self.get_pose(), self.occ_grid_map)
-
             else:
-                self.vel_ctrl = np.array([self.issued_v, self.issued_w])
+                self.u_t = np.array([self.issued_v, self.issued_w])
 
                 # Manual driving.
                 v1, v2 = Robot.__inverse_drive(
                     self.issued_v, self.issued_w, self.__b)
                 self.drive_direct(v1, v2)
+
+            if self.z_t is not None and np.sum(self.u_t) != 0:
+
+                tstart = time.time()
+                self.fast_slam.update(self.z_t, self.u_t)
+                print('fastSLAM update time:', time.time() - tstart)
+
+                self.z_t = None
+                self.u_t = None
 
             if self.is_thread_stop_requested[THREAD_MOTION]:
                 break
@@ -961,10 +981,10 @@ class Robot:
             read_w = delta_angle / delta_time    # Change in orientation (degree)
 
             self.__delta_distance = delta_distance
-            self.__delta_angle = delta_angle
+            self.__delta_angle = math.radians(delta_angle)
 
             # Update the position of the robot.
-            self.__update_odometry(delta_distance, delta_angle)
+            self.__update_odometry(delta_distance, self.__delta_angle)
 
             v1, v2 = 0, 0
 
@@ -1153,33 +1173,12 @@ class Robot:
 
         return (self.__v, self.__w)
 
-    def get_kinect_xy(self, slice_row):
-
-        # 480 x 640 ndarray matrix.
-        depth, _ = freenect.sync_get_depth()
-
-        u, v = np.mgrid[:depth.shape[1], slice_row:slice_row+1]
-
-        # xyz is an Nx3 array representing 3d coordinates of objects
-        # following standard right-handed coordinate system.
-        xyz, uv = calibkinect.depth2xyzuv(depth[v, u], u, v)
-
-        # Convert the 3d right-handed coordinate to the robot's 2d local 
-        # coordinate system.
-        x, y, z = xyz.T
-        xy = np.hstack((-z.T[:, np.newaxis], -x.T[:, np.newaxis]))
-
-        # Change from m to cm.
-        frame_local = xy * 100.0
-
-        return frame_local
-
     def get_delta_pose(self):
 
         dd = self.__delta_distance
-        da = self.__delta_angle # Degree
-        dx = dd * math.cos(math.radians(da))
-        dy = dd * math.sin(math.radians(da))
+        da = self.__delta_angle
+        dx = dd * math.cos(da)
+        dy = dd * math.sin(da)
         return np.array([dx, dy, da])
 
     def get_pose(self):
@@ -1213,16 +1212,16 @@ class Robot:
     def get_delta_angle(self):
 
         """
-        The change in angle in degrees.
+        The change in angle in radians.
         """
 
         return self.__delta_angle
 
     def get_delta_x(self):
-        return self.issued_v * math.cos(math.radians(self.__delta_angle))
+        return self.issued_v * math.cos(self.__delta_angle)
 
     def get_delta_y(self):
-        return self.issued_v * math.sin(math.radians(self.__delta_angle))
+        return self.issued_v * math.sin(self.__delta_angle)
 
     def enable_pid(self):
         self.is_pid_enable = True
@@ -1241,19 +1240,19 @@ class Robot:
             delta_distance:
                 The distance traveled within a consistent time tick.
             delta_angle:
-                The change in angle within a consistent time tick.
+                The change in angle within a consistent time tick in radians.
         """
 
         # Update orientation.
-        orientation = (self.__pose[2] + delta_angle) % 360
+        orientation = (self.__pose[2] + delta_angle) % (math.pi*2)
 
         # Convert the orientation from degree to radian for the next operations
         # (determining the x- and y-positon).
-        radian = (math.pi / 180.0) * orientation
+        # radian = (math.pi / 180.0) * orientation
 
         # Update x- and y-positon.
-        x = self.__pose[0] + delta_distance * math.cos(radian)
-        y = self.__pose[1] + delta_distance * math.sin(radian)
+        x = self.__pose[0] + delta_distance * math.cos(orientation)
+        y = self.__pose[1] + delta_distance * math.sin(orientation)
 
         self.__pose = np.array([x, y, orientation])
 

rutil.py
@@ -2,22 +2,17 @@ import math
 import numpy as np
 from scipy import ndimage
 
-def rigid_trans_mat3(pose, grids=False):
+def rigid_trans_mat3(pose):
 
     """
     Returns a rigid homogeneous transformation matrix.
 
     @param pose:
         A size-3 1-D numpy array [x, y, heading]^T where the heading is in
-        degrees.
+        radians.
     """
 
     x, y, r = pose
-    r = math.radians(r)
-
-    if grids:
-        y, x = -x, y
-        r = -r
 
     return np.array([
             [math.cos(r), -math.sin(r), x],
@@ -34,13 +29,21 @@ def transform_pts_2d(H, pts):
         A 3x3 homogenous transformation matrix.
     @param pts:
         An Nx2 numpy array. Each row in the array is a size-2 array representing
-        2-dimensional point.
+        real 2-dimensional point.
     """
 
     assert H.shape[0] == H.shape[1] and H.shape[0] == 3
     assert pts.shape[1] == 2
 
     return np.dot(H[:2,:2], pts.T).T + H[:2,2]
+
+def transform_cells_2d(H, cells, map_size, res):
+
+    mid = np.array(map_size) / 2
+
+    tr = np.array([-H[1,2], H[0,2]]) / res
+
+    return (np.dot(H[:2,:2], (cells - mid).T).T + mid + tr).astype(int)
 
 def transform_map(pose, grid_map):
 
@@ -146,8 +149,13 @@ def angle_between_vectors(vec_a, vec_b):
         np.dot(vec_a, vec_b) / (norm_a * norm_b))
 
 def angle_to_dir(angle):
-    x = math.cos(math.radians(angle))
-    y = math.sin(math.radians(angle))
+
+    """
+    Returns the direction vector from origin for the given angle in radians.
+    """
+
+    x = math.cos(angle)
+    y = math.sin(angle)
     return np.array([x, y])
 
 def disp_to_angle(disp):
@@ -158,7 +166,7 @@ def disp_to_angle(disp):
     if (x <= 0 and y <= 0) or (x >= 0 and y <= 0):
         a = 2 * math.pi - abs(a)
 
-    return math.degrees(a)
+    return a
 
 def log_odds_to_prob(k):
 
@@ -176,4 +184,7 @@ def prob_to_log_odds(p):
     """
 
     return math.log10(p / (1.0 - p))
+
+vec_log_odds_to_prob = np.vectorize(log_odds_to_prob)
+vec_prob_to_log_odds = np.vectorize(prob_to_log_odds)
 

slam.py
@@ -6,6 +6,8 @@ import time
 import bisect
 import rutil
 import scipy.stats as st
+import calibkinect
+import freenect
 from icp import icp
 from sklearn.neighbors import NearestNeighbors
 
@@ -46,14 +48,14 @@ class ParticleFilter:
 
         """
         @param z:
-            The 2-d real coordinates points projected by the depth map in the
+            The 2-d real coordinates points projected from the depth map in the
             local reference frame.
         @param u:
             The control command for the current time step. This is a size-2
             array with values [linear_velocity, and rotational_velocity].
         @param x:
             The current state (pose) of the robot. This is a size-3 array with
-            values [pos_x, pos_y, heading_degree]
+            values [pos_x, pos_y, heading_radian]
         @param m:
             The occupancy grid map, a 2-D numpy array.
         @param occ_thres:
@@ -61,6 +63,8 @@ class ParticleFilter:
         """
 
         X = []
+
+        z = z[::20]
 
         # observed_cells() use bresenham-line algorithm which is computationally
         # expensive large number of particles. Instead we can compute the local
@@ -81,8 +85,8 @@ class ParticleFilter:
                 u_noise = (1e-3, 1e-3, 3e-6, 3e-6, 3e-4, 3e-4)
                 x = sample_motion_model_velocity(u, p.x, self.deltatime,\
                         u_noise)
-                w = correlation_measurement_model(obsr_mat, p.x, m,
-                        self.resolution)
+                w = likelihood_field_measurement_model(z, p.x, obs_cells,
+                        m.shape, self.resolution)
             else:
                 w = 0
 
@@ -90,8 +94,12 @@ class ParticleFilter:
 
             X.append(p_)
 
+        # Effective sample size.
+        ess = len(self.particles) /(1 + ParticleFilter.cv(self.particles))
+
         # Resampling.
-        self.particles = ParticleFilter.low_variance_sampler(X)
+        if ess < 0.5*len(self.particles):
+            self.particles = ParticleFilter.low_variance_sampler(X)
 
     def low_variance_sampler(particles):
 
@@ -99,27 +107,83 @@ class ParticleFilter:
         Algorithm for reducing sampling error.
         """
 
-        X_ = []
-        n = len(particles)
-        r = np.random.uniform(0, 1 / n)
+        X = []
+        M = len(particles)
+        r = np.random.uniform(0, 1 / M)
         c = particles[0].w
 
         i = 0
-        for m in range(n):
-            u = r + m / n
+        for m in range(M):
+
+            u = r + m / M
             while u > c:
                 i = i + 1
                 c = c + particles[i].w
-            X_.append(particles[i])
 
-        return X_
+            new_x = np.copy(particles[i].x)
+            new_w = 1 / M
+
+            if particles[i].m is not None:
+                new_m = np.copy(particles[i].m)
+            else:
+                new_m = None
+
+            X.append(Particle(new_x, new_w, new_m))
+
+        return X
+
+    def select_with_replacement_resampler(particles):
+
+        X = []
+        M = len(particles)
+
+        # Calculate the cumulative weights of the particles.
+        q = np.cumsum([p.w for p in particles])
+
+        # Generate a sorted array of random numbers between 0.0 to 1.0.
+        r = np.sort(np.random.uniform(0, 1, size=(M + 1)))
+        r[M] = 1.0
+
+        i = j = 0
+        index = {}
+        while (i < M):
+            if (r[i] < q[j]):
+                index[i] = j
+                i = i + 1
+            else:
+                j = j + 1
+
+        for i in range(M):
+
+            new_x = np.copy(particles[index[i]].x)
+            new_w = 1 / M
+
+            if particles[i].m is not None:
+                new_m = np.copy(particles[i].m)
+            else:
+                new_m = None
+        
+            X.append(Particle(new_x, new_w, new_m))
+
+        return X
+
+    def cv(particles):
+
+        """
+        Returns the coefficient of variation of the particles.
+        """
+
+        M = len(particles)
+
+        return sum([(M*p.w - 1)**2 for p in particles]) / M
 
 class Particle:
 
-    def __init__(self, state, weight):
+    def __init__(self, state, weight, grid_map=None):
 
         self.x = state
         self.w = weight
+        self.m = grid_map
 
     def create_random(range_x, range_y, range_h, weight):
 
@@ -128,6 +192,74 @@ class Particle:
         h = np.random.uniform(range_h[0], range_h[1])
 
         return Particle(np.array([x, y, h]), weight)
+
+class FastSLAM:
+
+    def __init__(self, map_size, resolution, dt, num_particles=150):
+
+        self.MAP_SIZE = np.array(map_size)
+        self.RESOLUTION = resolution
+        self.M = num_particles
+        self.DELTA_TIME = dt
+        self.best_particle = 0
+
+        x = np.array([0, 0, 0])
+        w = 1 / num_particles
+        m = np.full(map_size, 0.5)
+
+        self.particles = [Particle(x, w, m) for _ in range(self.M)]
+
+    def update(self, z_t, u_t):
+
+        end_cells = world_frame_to_cell_pos(z_t, self.MAP_SIZE, self.RESOLUTION)
+        mid = np.array(self.MAP_SIZE // 2).astype(int)
+        obs_dict = observed_cells(mid, end_cells)
+
+        # An Nx3 matrix where each row is [row, col, val], and where val is the
+        # update to the belief on cell (row, col) on the occupancy grid map.
+        obs_mat = observation_matrix(obs_dict, occu=0.9, free=-0.7)
+
+        max_weight = -1
+        max_particle_index = 0
+
+        for i, p in enumerate(self.particles):
+
+            z_mat = np.copy(obs_mat)
+            z_cells = z_mat[:,:2]
+
+            # State transition of each particle.
+            x_prev = np.copy(p.x)
+            noise = (1e-2, 1e-8, 1e-8, 1e-2, 1e-10, 1e-10)
+            p.x = sample_motion_model_velocity(u_t, p.x, self.DELTA_TIME,
+                    noise=noise)
+            # print('x_t-1, u_t, x_t:', x_prev, u_t, p.x)
+
+            H = rutil.rigid_trans_mat3(p.x)
+            z_cells = rutil.transform_cells_2d(H, z_cells, self.MAP_SIZE,
+                    self.RESOLUTION)
+
+            # Compute the weight of this particle.
+            p.w = likelihood_field_measurement_model(z_cells, p.x,\
+                    np.argwhere(p.m > 0.75), self.MAP_SIZE, self.RESOLUTION)
+
+            if p.w > max_weight:
+                max_weight = p.w
+                max_particle_index = i
+
+            z_mat[:,:2] = z_cells
+
+            # Update the map of each particle.
+            update_occupancy_grid_map(p.m, z_mat)
+
+        self.best_particle = max_particle_index
+
+        # Effective sample size.
+        ess = self.M / (1 + ParticleFilter.cv(self.particles))
+
+        self.particles = ParticleFilter.low_variance_sampler(self.particles)
+
+    def highest_particle(self):
+        return self.particles[self.best_particle]
 
 def cell_to_world_pos(cell, map_size, resolution, center=False):
 
@@ -201,8 +333,7 @@ def world_frame_to_cell_pos(frame, map_size, resolution):
 def observed_cells(pos, end_pt_cells):
 
     """
-    Returns an Nx2 numpy array whose rows are the 2-D grid map coordinates of
-    all cells observed in the current scan, a size-N dictionary whose keys are
+    Returns size-N dictionary whose keys are
     the 2-D grid map coordinate each with either value 1 or 0 indicating
     occupied or free cell.
 
@@ -219,9 +350,9 @@ def observed_cells(pos, end_pt_cells):
     for x0, y0 in end_pt_cells:
         beam_line(cells, (x1, y1), (x0, y0))
 
-    return np.array(list(cells.keys())), cells
+    return cells
 
-def observation_matrix(cell_dict):
+def observation_matrix(cell_dict, occu=1, free=0):
 
     """
     Returns an Nx3 numpy array where each row [row, col, v] represents the row,
@@ -232,11 +363,14 @@ def observation_matrix(cell_dict):
         and v is the occupancy value on that cell.
     """
 
-    a = np.zeros((len(cell_dict), 3), dtype=int)
+    a = np.zeros((len(cell_dict), 3), dtype=float)
     for i, d in enumerate(cell_dict.items()):
         row, col = d[0]
-        val = d[1]
-        a[i] = [row, col, val]
+        if d[1]:
+            val = occu
+        else:
+            val = free
+        a[i] = [int(row), int(col), val]
     return a
 
 def observation_map(cell_dict, map_size):
@@ -368,8 +502,7 @@ def beam_line(cells,  p0, p1):
         else:
             __line_high(cells, p0, p1)
 
-def occupancy_grid_mapping(posterior, prev_posterior, pose, scan_data,
-    resolution):
+def occupancy_grid_mapping(posterior, prev_posterior, obs_dict):
 
     """
     Calculates the posterior distribution m over the map given the observation
@@ -402,19 +535,19 @@ def occupancy_grid_mapping(posterior, prev_posterior, pose, scan_data,
     # Bresenham's line algorithm to get the set of cells C_i that intersects
     # with a straight line formed between the robot and the given obstacle b_i
     # position.  Take the union C = C_0 U C_1 U ... U C_N.
-    cells = {}
-    x1, y1 = world_to_cell_pos(pose[:2], map_size, resolution)
-    for x0, y0 in scan_data:
-        beam_line(cells, (x1, y1), (x0, y0))
+    # cells = {}
+    # x1, y1 = world_to_cell_pos(pose[:2], map_size, resolution)
+    # for x0, y0 in scan_data:
+    #     beam_line(cells, (x1, y1), (x0, y0))
 
     # Perform log odds update on the posterior map distribution.
-    for p in cells.keys():
+    for p in obs_dict.keys():
 
         if is_out_of_bound(p, map_size):
             continue
 
         # p was observed as an obstacle.
-        if cells[p]:
+        if obs_dict[p]:
             logit = rutil.prob_to_log_odds(prev_posterior[p]) + OCCU
             posterior[p] = rutil.log_odds_to_prob(logit)
 
@@ -422,6 +555,34 @@ def occupancy_grid_mapping(posterior, prev_posterior, pose, scan_data,
         else:
             logit = rutil.prob_to_log_odds(prev_posterior[p]) + FREE
             posterior[p] = rutil.log_odds_to_prob(logit)
+
+def update_occupancy_grid_map(m, obs_mat):
+
+    rows, cols = obs_mat[:,0].astype(int), obs_mat[:,1].astype(int)
+
+    tmp = rutil.vec_prob_to_log_odds(m[rows, cols]) + obs_mat[:,2]
+    m[rows, cols] = rutil.vec_log_odds_to_prob(tmp)
+
+def local_occupancy(cell_dict, out, map_size, resolution):
+
+    OCCU = 0.9
+    FREE = -0.7
+
+    for p in cell_dict.keys():
+        
+        if is_out_of_bound(p, map_size):
+            continue
+
+        if p not in out:
+            out[p] = 0.5
+
+        if cell_dict[p]:
+            logit = rutil.prob_to_log_odds(out[p]) + OCCU
+            out[p] = rutil.log_odds_to_prob(logit)
+
+        else:
+            logit = rutil.prob_to_log_odds(out[p]) + FREE
+            out[p] = rutil.log_odds_to_prob(logit)
 
 def scan_match(src_pts, dst_pts, pts):
 
@@ -468,9 +629,6 @@ def motion_model_velocity(succ_pose, curr_pose, control, dt,
     x1, y1, h1 = succ_pose 
     x0, y0, h0 = curr_pose 
 
-    h1 = math.radians(h1)
-    h0 = math.radians(h0)
-
     dx = x0 - x1
     dy = y0 - y1
 
@@ -503,7 +661,7 @@ def motion_model_velocity(succ_pose, curr_pose, control, dt,
     return p1*p2*p3
 
 def sample_motion_model_velocity(control, curr_pose, dt,
-    noise=(1, 1, 1, 1, 1, 1)):
+    noise=(1e-10, 1e-10, 1e-10, 1e-10, 1e-10, 1e-10)):
 
     """
     Sample x_t from the probability p(x_t | u_t, x_{t-1}). This can be used to
@@ -528,7 +686,6 @@ def sample_motion_model_velocity(control, curr_pose, dt,
     a1, a2, a3, a4, a5, a6 = noise
 
     x, y, h = curr_pose
-    h = math.radians(h)
 
     v, w = control
 
@@ -545,10 +702,58 @@ def sample_motion_model_velocity(control, curr_pose, dt,
     y1 = y + r_*math.cos(h) - r_*math.cos(h + w_*dt)
     h1 = h + w_*dt + gamma*dt
 
-    return np.array([x1, y1, np.degrees(h1)])
+    return np.array([x1, y1, h1])
 
-def likelihood_field_measurement_model(z, x, gmap, occ_cells, res,
-        mean=6.3, sd=2.31458)
+# def sample_motion_model_odometry(u_t, prev_x,\
+#     noise=(1e-10, 1e-10, 1e-10, 1e-10)):
+
+#     assrt len(noise) == 4
+
+#     a1, a2, a3, a4 = noise
+
+#     odom_pose1, odom_pose2 = u_t[0], u_t[1]
+
+#     x1, y1, h1 = odom_pose1
+#     x2, y2, h2 = odom_pose2
+
+#     d_rot1 = math.atan2(y2 - y1, x2 - x1)
+#     d_trans = math.sqrt((x2 - x1)**2, (y2 - y1)**2)
+#     d_rot2 = h2 - h1 - d_rot1
+
+#     d_rot1_sq = d_rot1**2
+#     d_trans_sq = d_trans**2
+#     d_rot2_sq = d_rot2**2
+
+#     d_rot1_ = d_rot1 - sample_normal_distribution(a1*d_rot1_sq + a2*d_trans_sq)
+#     d_trans_ = d_trans - sample_normal_distribution(\
+#         a3*d_trans_sq + a4*d_rot1_sq + a4*d_rot2_sq)
+#     d_rot2_ = d_rot2 - sample_normal_distribution(a1*d_rot2_sq + a2*d_trans_sq)
+
+#     return
+
+def sample_motion_model_odometry(u_t, prev_x,\
+    noise=(1e-10, 1e-10, 1e-10, 1e-10)):
+
+    assert len(noise) == 4
+
+    a1, a2, a3, a4 = noise
+
+    d_dist, d_rad = u_t
+
+    d_dist_sq = d_dist**2
+    d_rad_sq = d_rad**2
+
+    d_dist_ = d_dist - sample_normal_distribution(a1*d_rad_sq + a2*d_dist_sq)
+    d_rad_ = d_rad - sample_normal_distribution(a3*d_dist_sq + a4*d_rad_sq)
+
+    x, y, h = prev_x
+    x = x + d_dist_ * math.cos(h + d_rad_)
+    y = y + d_dist_ * math.sin(h + d_rad_)
+    h = h + d_rad_
+
+    return np.array([x, y, h])
+
+def likelihood_field_measurement_model(end_cells, x, occ_cells, map_size, res):
 
     """
     Compute the probability p(z_t, | x_t, m).
@@ -559,39 +764,25 @@ def likelihood_field_measurement_model(z, x, gmap, occ_cells, res,
         The state or pose of the particle.
     @param occ_cells:
         An Mx2 array for M obstacle cells in the occupancy grid map.
-    @param mean:
-        The expected (mean value) of the mean distance between obstacle points
-        from scan data and the obstacle points on the map.
-    @param sd:
-        The standard deviation of the distances d_i where d_i is the nearest
-        distance of each observed obstacle points p_i to the nearest obstacle
-        points P_i on the map when the mean value is mean (param).
     """
 
     if len(occ_cells) == 0:
         return 0
 
-    # Transform z to be in the global reference frame.
     H = rutil.rigid_trans_mat3(x)
-    z = rutil.transform_pts_2d(H, z)
+    end_cells = rutil.transform_cells_2d(H, end_cells, map_size, res)
 
-    occ_pts = cell_to_world_pos(occ_cells, gmap.shape, res, center=True)
+    nbrs = NearestNeighbors(n_neighbors=1, algorithm='auto').fit(end_cells)
 
-    nbrs = NearestNeighbors(n_neighbors=1, algorithm='auto').fit(occ_pts)
+    dist, inds = nbrs.kneighbors(occ_cells)
 
-    dist, inds = nbrs.kneighbors(z)
+    ave_min_dist = 0
+    for d in dist[0]:
+        ave_min_dist += d
 
-    m = np.mean(dist)
+    return np.exp(-ave_min_dist / len(dist))
 
-    Z = (m - mean) / sd
-
-    # Find the probability that random variable m is within 
-    # +/- one standard deviation away from the zero-centered mean.
-    prob = st.norm(0, 1).cdf(Z + sd) - st.norm(0, 1).cdf(Z - sd)
-
-    return prob
-
-def correlation_measurement_model(obsr_mat, pose, grid_map, resolution):
+def correlation_measurement_model(obsr_mat, grid_map, resolution):
 
     """
     Compute the probability p(z_t | x_t, m) i.e., the probability that the
@@ -605,24 +796,21 @@ def correlation_measurement_model(obsr_mat, pose, grid_map, resolution):
          See also https://en.wikipedia.org/wiki/Pearson_correlation_coefficient
     """
 
-    z = cell_to_world_pos(obsr_mat[:,:2], grid_map.shape, resolution)
-    H = rutil.rigid_trans_mat3(pose)
-    z = rutil.transform_pts_2d(H, z)
-
     n = obsr_mat.shape[0]
 
     glob_mat = np.zeros((n, 3))
     for i, k in enumerate(obsr_mat):
         row, col, val = k
+        row, col = int(row), int(col)
         glob_mat[i] = [row, col, grid_map[row, col]]
 
     m = np.sum(obsr_mat[:,2] + glob_mat[:,2]) / (2*n)
     a = np.sum((glob_mat[:,2] - m) * (obsr_mat[:,2] - m))
     b = np.sum((glob_mat[:,2] - m)**2)
     c = np.sum((obsr_mat[:,2] - m)**2)
-    ppc = a / (math.sqrt(b*c))
+    pcc = a / (math.sqrt(b*c))
 
-    return max(0, ppc)
+    return max(0, pcc)
 
 def correlation_measurement_model2(obs_map, pose, grid_map, mask, resolution):
 
@@ -667,7 +855,12 @@ def sample_normal_distribution(b_sq):
 
 def prob_normal_distribution(a, b_sq):
 
-    return (1 / math.sqrt(2*math.pi*b_sq)) * math.exp(-0.5*(a**2) / b_sq)
+    """
+    Finds the probability of argument a under a zero-centered distribution with
+    variance b_sq.
+    """
+
+    return math.exp(-(a**2) /  (2*b_sq)) / math.sqrt(2*math.pi*b_sq)
 
 def posterior_prob_dist(posterior):
 
@@ -693,3 +886,26 @@ def d3_map(posterior, invert=False):
         d = 255 - d
 
     return d
+
+def get_kinect_xy(slice_row):
+
+    # 480 x 640 ndarray matrix.
+    depth, _ = freenect.sync_get_depth()
+
+    u, v = np.mgrid[:depth.shape[1], slice_row:slice_row+1]
+
+    # xyz is an Nx3 array representing 3d coordinates of objects
+    # following standard right-handed coordinate system.
+    xyz, uv = calibkinect.depth2xyzuv(depth[v, u], u, v)
+
+    # Convert the 3d right-handed coordinate to the robot's 2d local 
+    # coordinate system.
+    x, y, z = xyz.T
+    xy = np.hstack((-z.T[:, np.newaxis], -x.T[:, np.newaxis]))
+
+    # Change from m to cm.
+    frame_local = xy * 100.0
+    depth_data = depth[v, u].flatten()
+    depth_data = depth_data[depth_data < 2047] * 0.1
+
+    return frame_local, depth_data