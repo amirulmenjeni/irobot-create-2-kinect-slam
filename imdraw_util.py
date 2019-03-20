b5cda5206683130253d1cb06de7bd947a4d559c9
Amirul Menjeni <amirulmenjeni@gmail.com>
2019-03-21 00:31:12 +0800

    Add border to robot drawing on map.

--- imdraw_util.py
+++ imdraw_util.py
@@ -44,13 +44,12 @@ def draw_horizontal_line(d3_map, y_pos, bgr):
         d3_map[y_pos, :, k] = bgr[k]
 
 def draw_robot(d3_map, resolution, pose, bgr=(255, 255, 0), radius=5,
-        show_heading=True):
+        show_heading=True, heading_thickness=1, border_thickness=-1,
+        border_bgr=(0, 0, 0)):
+
 
     map_size = d3_map.shape[:2]
     y0, x0 = slam.world_to_cell_pos(pose[:2], map_size, resolution)
-
-    # Draw circle for body.
-    cv2.circle(d3_map, (x0, y0), radius, bgr, thickness=-1)
 
     # Draw a line for heading.
     if show_heading:
@@ -59,4 +58,12 @@ def draw_robot(d3_map, resolution, pose, bgr=(255, 255, 0), radius=5,
         H = rutil.rigid_trans_mat3(np.array([0, 0, pose[2]]))
         p1 = p0 + rutil.transform_pts_2d(H, np.array([dd]))[0]
         y1, x1 = slam.world_to_cell_pos(p1, map_size, resolution)
-        cv2.line(d3_map, (x0, y0), (x1, y1), bgr)
+
+        # Draw line (to show heading).
+        cv2.line(d3_map, (x0, y0), (x1, y1), border_bgr,
+                thickness=border_thickness+1)
+        cv2.line(d3_map, (x0, y0), (x1, y1), bgr, thickness=1)
+        
+    # Draw circle for body.
+    cv2.circle(d3_map, (x0, y0), radius, bgr, thickness=-1)
+    cv2.circle(d3_map, (x0, y0), radius, border_bgr, thickness=border_thickness)
