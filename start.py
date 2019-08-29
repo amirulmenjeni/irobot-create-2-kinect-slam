397f4e34a660569f08beb4b28d0c5dacaefe784e
Amirul Menjeni <amirulmenjeni@gmail.com>
2019-08-29 16:58:45 +0800

    Add cli argument to start.py script.

--- start.py
+++ start.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append('../')
 
+import argparse
 import time
 import slam
 import cv2
@@ -12,6 +13,16 @@ import numpy as np
 import config
 import logging
 
+parser = argparse.ArgumentParser(description='Start robot.')
+parser.add_argument('--show-display', help='Show the map display, only work if
+        this script is run locally (e.g., not over ssh connection).',\
+        default=True, action='store_true')
+parser.add_argument('--disable-auto', help='Disable autonomous driving.',\
+        default=False, action='store_true')
+parser.add_argument('--usb-port', help='The USB port to the roomba robot.',\
+        default='/dev/ttyUSB0')
+args = parser.parse_args()
+
 r = Robot()
 r.drive_velocity(0, 0)
-r.run(show_display=True, disable_auto=False)
+r.run(show_display=args.show_display, disable_auto=args.disable_auto)
